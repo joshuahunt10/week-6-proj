a9454a60ef9c273433a7bc84c2ccce5282a0d3b5
joshuahunt10 <joshua.hunt88@gmail.com>
2017-07-01 13:21:05 -0500

    login authentication done

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const mustache = require("mustache-express");
 const models = require("./models")
 const bodyparser = require("body-parser");
 const faker = require("faker")
+const session = require('express-session');
 
 
 app.engine('mustache', mustache());
@@ -15,14 +16,49 @@ app.listen(3000, function(){
   console.log('I created Facebook in a week');
 })
 
-for (var i = 0; i < posts.length; i++) {
-  posts[i]
-}
+app.use(session({
+  secret: 'puppers',
+  resave: false,
+  saveUninitialized: true
+}));
+
+// var user = models.Username.build({
+//   username: 'josh',
+//   password: '123'
+// })
+// user.save();
+
+app.post('/authenticate', function(req, res){
+  let user = false;
+  username = req.body.username;
+  password = req.body.password;
+  let userQuery = models.Username.findOne({
+    where:{
+      username: username
+    }
+  }).then(function(userQuery){
+      if(username === userQuery.username){
+        if(password === userQuery.password){
+          console.log('login success');
+          user = userQuery.username;
+        }
+      }
+      if(user){
+        req.session.user = userQuery.username;
+        console.log('logging the session', req.session.user);
+        res.redirect("/")
+      }else{
+        res.render('loginerror');
+      }
+  })
+
+})
 
 
 app.get('/', function(req, res){
   res.render('index', {
-    title: 'Home Page!!'
+    title: 'Home Page!!',
+    user: req.session.user
   });
 })
 
