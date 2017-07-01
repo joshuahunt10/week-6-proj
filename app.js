8e41ae25530dd7a540c759355809e234fe77961b
joshuahunt10 <joshua.hunt88@gmail.com>
2017-07-01 11:36:41 -0500

    styling done

--- app.js
+++ app.js
@@ -15,6 +15,11 @@ app.listen(3000, function(){
   console.log('I created Facebook in a week');
 })
 
+for (var i = 0; i < posts.length; i++) {
+  posts[i]
+}
+
+
 app.get('/', function(req, res){
   res.render('index', {
     title: 'Home Page!!'
---
+++ models/post.js
@@ -0,0 +1,15 @@
+'use strict';
+module.exports = function(sequelize, DataTypes) {
+  var Post = sequelize.define('Post', {
+    title: DataTypes.STRING,
+    body: DataTypes.TEXT,
+    user: DataTypes.INTEGER
+  }, {
+    classMethods: {
+      associate: function(models) {
+        // associations can be defined here
+      }
+    }
+  });
+  return Post;
+};
